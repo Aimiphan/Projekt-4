5c04a097e6345e1fc4b1eb2d440a6c17fb978e0c
khni48704 <khni48704@edu.ucl.dk>
2023-04-12 12:30:29 +0200

    js kasper

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 //Kasper
+let billedergalleri = [
+    ["img/rød.png","img/gul.png","img/grøn.png"]
+]
+
+let select_element = document.getElementById("");
+for(let i = 0; <galleri.lenght; i++) {
+
+    let billedergalleri = galleri [i];
+    select_element.innerHTML +="<div class'billederigalleri' > + '" billedegaleri[i][0]+ billedergalleri[i][1]++ billedergalleri[i][2]    
+}
 
 
 //Signe
