ca81484c43ed2aac4d6bd0de9ef4ad13bc8aab4d
SigneWehlast <sswe48620@edu.ucl.dk>
2023-04-12 12:30:22 +0200

    id countdown

--- script.js
+++ script.js
@@ -2,6 +2,29 @@
 
 
 //Signe
+let countDownBowling = new Date("May 5, 2023 17:00:00").getTime();
+
+let x = setInterval(function() {
+
+    let nu = new Date().getTime();
+    
+    let dageIndtil = countDownBowling - nu;
+
+    let dage = Math.floor(dageIndtil / (1000 * 60 * 60 * 24));
+
+    let timer = Math.floor((dageIndtil % (1000 * 60 * 60 * 24)) / (1000 * 60 *60));
+
+    let minutter = Math.floor((dageIndtil % (1000 * 60 * 60 * 24)) / (1000 * 60));
+
+    let sekunder = Math.floor((dageIndtil % (1000 * 60)) / 1000);
+
+    document.getElementById("countdownid").innerHTML = dage + "d " + timer + "t " + minutter + "m " + sekunder + "s ";
+
+    if (dageIndtil < 0 ) {
+        clearInterval(x);
+        document.getElementById("countdownid").innerHTML = "Eventet er igang";
+    }
+}, 1000);
 
 //Aimi
 
